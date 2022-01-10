2f9f64a5f2abb1263ae2e5ca9600ed610ca77a83
Alexis Ocampo <al3xisoc4mpo@gmail.com>
2022-01-10 12:57:41 -0600

    Base

---
+++ index.js
@@ -0,0 +1,71 @@
+// GENERAR COMENTARIO "//" DOBLE DIAGONAL
+
+// GENERAR MENSAJES A LA TERMINAL DEL CLIENTE (CUALQUIER NAVEGADOR)
+console.log ('Hola Mundo desde Javascript')
+
+// VARIABLES
+// PRIMITIVOS
+// 1. Texto plano
+// var - JAVASCRIP TRADICIONAL (ES5)
+var nombre = 'Hola soy Alexis Ocampo';
+console.log(nombre);
+// let
+// forma de crear variables el cual su contenido puede ser mutable
+let saludo = 'Hola como estan?'
+saludo = 'How are you?'
+console.log(saludo)
+// const
+// forma de crear variables pero el contenido va a ser estatico y fijo
+let iva = 16;
+iva = 10;
+console.log (iva);
+
+// 2. Number - Numero
+const age = 34;
+console.log (age);
+
+
+// 3. Boolean - True or false
+const isRaining = false;
+console.log ("Esta lloviendo", isRaining);
+
+// 4. Undefined - No esta definido aun, pero no lo estara.
+
+let price;
+console.log ('Precio indefinido',price);
+
+price = 15;
+console.log ('Precio ya definido', price);
+
+// 5. null - nulo - Ya se definio y es un valor inexistente. Ausencia de valor.
+
+const hasEngine = null;
+
+// 1. Texto plano
+const apellido = 'Ocampo'
+console.log (apellido);
+
+// 2. Number - Numero
+const titulos = 1;
+console.log (age);
+
+// 3. Boolean - True or false
+const isMotivated = true;
+console.log ("Esta motivado?", isRaining);
+
+// 4. Undefined - No esta definido aun, pero no lo estara.
+
+let fruit = undefined;
+console.log ('Fruta indefinida', fruit);
+
+fruit = 'Manzana';
+console.log ('Fruta ya definido', fruit);
+
+// 5. null - nulo - Ya se definio y es un valor inexistente. Ausencia de valor.
+
+const hasRobotArms = null;
+
+// REGLA DE VIDA EN JS
+// Siempre usar const a menos que necesites mutarlo
+
+// NO PRIMITIVOS
